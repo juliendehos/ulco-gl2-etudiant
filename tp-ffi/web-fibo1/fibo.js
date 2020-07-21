95e2bce22572178c1fe06c0900323b8c22ed21b9
Julien Dehos <dehos@univ-littoral.fr>
2020-07-21 10:07:43 +0200

    ffi web-fibo1

---
+++ tp-ffi/web-fibo1/fibo.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function fiboNaive (n) {
+    if (n < 2)
+        return n;
+    else
+        return fiboNaive(n-1) + fiboNaive(n-2);
+}
+
+function fiboIterative (n) {
+    if (n < 2)
+        return n;
+    else {
+        let f0 = 0;
+        let f1 = 1;
+        for (let i=1; i<n; i++) {
+            const tmp = f1+f0;
+            f0 = f1;
+            f1 = tmp;
+        }
+        return f1;
+    }
+}
+
