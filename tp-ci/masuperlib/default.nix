3611393e28521f4beacbaef00680caa476dc06da
Julien Dehos <dehos@univ-littoral.fr>
2020-07-22 12:02:32 +0200

    ci masuperlib

--- tp-ci/masuperlib/default.nix
+++ tp-ci/masuperlib/default.nix
@@ -9,10 +9,10 @@ pkgs.clangStdenv.mkDerivation {
         pkgs.catch2
     ];
 
-    # doCheck = true;
-    # checkPhase = ''
-    #   LD_LIBRARY_PATH="$(pwd)" ctest --force-new-ctest-process
-    # '';
+    doCheck = true;
+    checkPhase = ''
+      LD_LIBRARY_PATH="$(pwd)" ctest --force-new-ctest-process
+    '';
 
 }
 
