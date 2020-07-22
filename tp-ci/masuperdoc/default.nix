eb153283c41d9d0e6b27e82077d43016a8ca3a20
Julien Dehos <dehos@univ-littoral.fr>
2020-07-22 15:55:34 +0200

    ci masuperdoc

--- tp-ci/masuperdoc/default.nix
+++ tp-ci/masuperdoc/default.nix
@@ -4,24 +4,19 @@ stdenv.mkDerivation {
   name = "masuperdoc";
   src = ./.;
 
-  installPhase = ''
-    mkdir -p $out/public
-    cp index.html $out/public/
+  nativeBuildInputs = [
+    gnumake
+    python3Packages.sphinx
+  ];
+
+  buildPhase = ''
+    make html
   '';
 
-  # nativeBuildInputs = [
-  #   gnumake
-  #   python3Packages.sphinx
-  # ];
-
-  # buildPhase = ''
-  #   make html
-  # '';
-
-  # installPhase = ''
-  #   mkdir -p $out/public
-  #   cp -r build/html/* $out/public/
-  # '';
+  installPhase = ''
+    mkdir -p $out/public
+    cp -r build/html/* $out/public/
+  '';
 
 }
 
