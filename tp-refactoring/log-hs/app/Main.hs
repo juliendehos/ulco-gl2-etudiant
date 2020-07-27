9c5ef161fa8c3714679c75356185af5ee2fe7f46
Julien Dehos <dehos@univ-littoral.fr>
2020-07-27 17:27:32 +0200

    refactoring log-hs

--- tp-refactoring/log-hs/app/Main.hs
+++ tp-refactoring/log-hs/app/Main.hs
@@ -1,5 +1,3 @@
-import System.IO
-
 add3 :: Int -> Int 
 add3 = (+3)
 
@@ -14,23 +12,9 @@ mycompute v0 = do
     let v2 = mul2 v1
     return v2
 
-mycomputeFile :: Handle -> Int -> IO Int
-mycomputeFile h v0 = do
-    hPutStrLn h ("add3 " ++ show v0)
-    let v1 = add3 v0
-    hPutStrLn h ("mul2 " ++ show v1)
-    let v2 = mul2 v1
-    return v2
-
 main :: IO ()
 main = do
     putStrLn "this is log-hs"
     
     res <- mycompute 18
     print res
-
-    h <- openFile "log.txt" WriteMode
-    resFile <- mycomputeFile h 18
-    print resFile
-    hClose h
-
