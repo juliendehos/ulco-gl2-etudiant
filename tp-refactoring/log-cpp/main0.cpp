0f907128d354fa0c04e3e0579aacce10f4b23d55
Julien Dehos <dehos@univ-littoral.fr>
2020-07-27 18:22:22 +0200

    refactoring log-cpp

--- tp-refactoring/log-cpp/main0.cpp
+++ tp-refactoring/log-cpp/main0.cpp
@@ -18,23 +18,11 @@ int mycompute(int v0) {
     return v2;
 }
 
-int mycomputeFile(std::ostream & os, int v0) {
-    os << "add3 " + std::to_string(v0) << std::endl;
-    const int v1 = add3(v0);
-    os << "mul2 " + std::to_string(v1) << std::endl;
-    const int v2 = add3(v1);
-    return v2;
-}
-
 int main() {
     std::cout << "this is log-cpp" << std::endl;
 
     const int res = mycompute(18);
     std::cout << res << std::endl;
-
-    std::ofstream ofs("log.txt");
-    const int resFile = mycomputeFile(ofs, 18);
-    std::cout << resFile << std::endl;
 
     return 0;
 }
---
+++ tp-refactoring/log-cpp/main3.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <functional>
+#include <iostream>
+
+int add3(int n) {
+    return n+3;
+}
+
+int mul2(int n) {
+    return n*2;
+}
+
+struct AbstractMycompute {
+    virtual ~AbstractMycompute();
+    virtual int mycompute(int v0) = 0;
+};
+
+struct Mycompute : AbstractMycompute {
+    int mycompute(int v0) override {
+        std::cout << "add3 " + std::to_string(v0) << std::endl;
+        const int v1 = add3(v0);
+        std::cout << "mul2 " + std::to_string(v1) << std::endl;
+        const int v2 = add3(v1);
+        return v2;
+    }
+};
+
+int main() {
+    std::cout << "this is log-cpp" << std::endl;
+
+    Mycompute mc;
+    const int res = mc.mycompute(18);
+    std::cout << res << std::endl;
+
+    return 0;
+}
+
