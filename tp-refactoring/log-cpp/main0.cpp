9aafbd5e5ddd33ab2509466e67b68345f588cb34
Julien Dehos <dehos@univ-littoral.fr>
2020-07-27 18:14:06 +0200

    refactoring log-cpp

---
+++ tp-refactoring/log-cpp/main0.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <functional>
+#include <iostream>
+
+int add3(int n) {
+    return n+3;
+}
+
+int mul2(int n) {
+    return n*2;
+}
+
+int mycompute(int v0) {
+    std::cout << "add3 " + std::to_string(v0) << std::endl;
+    const int v1 = add3(v0);
+    std::cout << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    return v2;
+}
+
+int mycomputeFile(std::ostream & os, int v0) {
+    os << "add3 " + std::to_string(v0) << std::endl;
+    const int v1 = add3(v0);
+    os << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    return v2;
+}
+
+int main() {
+    std::cout << "this is log-cpp" << std::endl;
+
+    const int res = mycompute(18);
+    std::cout << res << std::endl;
+
+    std::ofstream ofs("log.txt");
+    const int resFile = mycomputeFile(ofs, 18);
+    std::cout << resFile << std::endl;
+
+    return 0;
+}
+
+
