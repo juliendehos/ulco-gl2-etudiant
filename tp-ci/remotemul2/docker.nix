1c7ff08db0ac87d48972b4f1b5a9a2f15584abc5
Julien Dehos <dehos@univ-littoral.fr>
2021-03-03 12:48:30 +0100

    ci docker

--- tp-ci/remotemul2/docker.nix
+++ tp-ci/remotemul2/docker.nix
@@ -1,7 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  app = pkgs.haskellPackages.callCabal2nix "remotemul2" ./. {};
+  # app = pkgs.haskellPackages.callCabal2nix "remotemul2" ./. {};
+  drv = pkgs.haskellPackages.callCabal2nix "remotemul2" ./. {};
+  app = pkgs.haskell.lib.justStaticExecutables drv;
 
   entrypoint = pkgs.writeScript "entrypoint.sh" ''
     #!${pkgs.stdenv.shell}
