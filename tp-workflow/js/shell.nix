249e3b11f729bb62f3f0fe066520639c41ddb694
Julien Dehos <dehos@univ-littoral.fr>
2021-08-25 17:05:39 +0200

    jpp de js

--- tp-workflow/js/shell.nix
+++ tp-workflow/js/shell.nix
@@ -1,4 +1,4 @@
-with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a9d3d2f349ae01c46f9e506502b494f0ee2caa98.tar.gz") {};
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz") {};
 
 emscriptenStdenv.mkDerivation {
   name = "tictactoe-wasm";
