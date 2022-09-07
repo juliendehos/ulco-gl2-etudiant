6d569fe5a9ab5575043f26e0dd13e67d627b0e13
Julien Dehos <dehos@univ-littoral.fr>
2022-09-07 22:16:51 +0200

    ci masuperlib

---
+++ tp-ci/masuperlib/test/masuperlib/masuperlib-test.cpp
@@ -0,0 +1,12 @@
+#include <masuperlib/masuperlib.hpp>
+
+#include <catch2/catch.hpp>
+
+TEST_CASE( "mul2 0" ) {
+    REQUIRE( masuperlib::mul2(0) == 0 );
+}
+
+TEST_CASE( "mul2 21" ) {
+    REQUIRE( masuperlib::mul2(21) == 0 );
+}
+
