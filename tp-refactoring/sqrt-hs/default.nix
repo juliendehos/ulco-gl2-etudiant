637f2bd16127c0d17479748249d15d78200c0bed
Julien Dehos <dehos@univ-littoral.fr>
2023-08-24 12:17:47 +0200

    tp refactoring

--- tp-refactoring/sqrt-hs/default.nix
+++ tp-refactoring/sqrt-hs/default.nix
@@ -1,4 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
-let drv = pkgs.haskellPackages.callCabal2nix "sqrt-hs" ./. {};
-in if pkgs.lib.inNixShell then drv.env else drv
+
+let
+  ghc = pkgs.haskellPackages;
+
+in ghc.developPackage {
+  root = ./.;
+  withHoogle = false;
+
+  modifier = drv:
+    #pkgs.haskell.lib.dontHaddock (
+      pkgs.haskell.lib.addBuildTools drv (with ghc; [
+        cabal-install
+    ]);
+    #]));
+
+}
+
 
