9f4748e74f3d9f183f334d1b9eed4c762612492d
Julien Dehos <dehos@univ-littoral.fr>
2025-03-05 08:29:03 +0100

    gl2, ffi, config nix

---
+++ tp-ffi/hs-c/default.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  ghc = pkgs.haskellPackages;
+
+in ghc.developPackage {
+  root = ./.;
+  withHoogle = false;
+
+  modifier = drv:
+    #pkgs.haskell.lib.dontHaddock (
+      pkgs.haskell.lib.addBuildTools drv (with ghc; [
+        cabal-install
+    ]);
+    #]));
+
+}
+
+
