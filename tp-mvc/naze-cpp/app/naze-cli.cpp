a96a331e939fe384218af32e507c85e14bd053c0
Julien Dehos <dehos@univ-littoral.fr>
2025-03-25 15:00:12 +0100

    tp-mvc, naze-hs + naze-cpp, unbuffered stdin

--- tp-mvc/naze-cpp/app/naze-cli.cpp
+++ tp-mvc/naze-cpp/app/naze-cli.cpp
@@ -2,10 +2,39 @@
 #include <Board.hpp>
 
 #include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+int mygetch() {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
 
 int main() {
 
     // TODO
+
+    // clear screen
+    std::cout << "\033[2J\033[1;1H";
+
+    while (true) {
+        int c = mygetch();
+        std::cout << "\033[2J\033[1;1H";
+        switch (c) {
+            case 's': std::cout << "Down";  break;
+            case 'w': std::cout << "Up";    break;
+            case 'a': std::cout << "Left";  break;
+            case 'd': std::cout << "Right"; break;
+            case 'q': exit(0);
+        }
+    }
 
     return 0;
 }
--- tp-mvc/naze-hs/app/naze-cli.hs
+++ tp-mvc/naze-hs/app/naze-cli.hs
@@ -3,12 +3,34 @@ import System.IO
 
 -- import Model
 
+-- get special keys
+getKey :: IO String
+getKey = reverse <$> go ""
+    where
+        go chars = do
+            char <- getChar
+            more <- hReady stdin
+            (if more then go else return) (char : chars)
+
+run :: IO ()
+run = do
+    key <- getKey
+
+    -- clear screen
+    putStrLn "\ESC[2J\ESC[1;1H"
+    
+    -- check arrow keys
+    case key of
+        "\ESC[A" -> putStrLn "Up" >> run
+        "\ESC[B" -> putStrLn "Down" >> run
+        "\ESC[C" -> putStrLn "Left" >> run
+        "\ESC[D" -> putStrLn "Right" >> run
+        "q" -> return ()
+        _ -> run
+
 main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering
-    putStr "> "
-    hFlush stdout
-    c <- getChar
-    putStrLn ""
-    print c
+    putStrLn "\ESC[2J\ESC[1;1H"
+    run
 
